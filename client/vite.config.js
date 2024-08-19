387806300d7b39b107ec2fed5c1d49dac0b73fcb
Aditya Singh <sengaraditya01@gmail.com>
2024-08-19 06:06:31 +0530

    removing proxies

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": "http://localhost:8800",
-    },
+    // proxy: {
+    //   "/api": "http://localhost:8800",
+    // },
   },
   plugins: [react()],
 });
