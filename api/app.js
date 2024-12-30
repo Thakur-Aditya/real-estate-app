aa25293dc79536f63b5e8b4f2a86294faa6cf775
Thakur-Aditya <87617889+Thakur-Aditya@users.noreply.github.com>
2024-12-30 10:30:38 +0530

    updated cors

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
 // app.use(cors({ origin: "http://localhost:5173/api", credentials: true })); //Credentials for sending cookies
-app.use(cors({ origin: "https://real-estate-app-one-beta.vercel.app/", credentials: true })); //Credentials for sending cookies
+app.use(cors({ origin: "https://real-estate-app-beta-six.vercel.app/", credentials: true })); //Credentials for sending cookies
 app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
