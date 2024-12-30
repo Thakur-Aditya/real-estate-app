6bd98ea6d5ff09582196fa31fd2b8d4eb3e297f4
Thakur-Aditya <87617889+Thakur-Aditya@users.noreply.github.com>
2024-12-30 10:24:07 +0530

    added vercel.json + bugs fixed

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
     // baseURL: "http://localhost:8800/api",
-  baseURL: "https://real-estate-app-q2wd.onrender.com/api",
+//   baseURL: "https://real-estate-app-q2wd.onrender.com/api",
 
     
     withCredentials: true, //for cookies
